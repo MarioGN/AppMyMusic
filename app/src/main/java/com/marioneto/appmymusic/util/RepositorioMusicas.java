ff5f10b10287aa035bb71fb0fab63b018c572cc3
MarioGN <mario.geroldineto@gmail.com>
2019-03-21 00:46:10 -0300

    criado repositório de músicas

---
+++ app/src/main/java/com/marioneto/appmymusic/util/RepositorioMusicas.java
@@ -0,0 +1,25 @@
+package com.marioneto.appmymusic.util;
+
+import com.marioneto.appmymusic.bean.Catalogo;
+import com.marioneto.appmymusic.bean.Genero;
+import com.marioneto.appmymusic.bean.Musica;
+
+class RepositorioMusicas {
+    static private final Catalogo catalogo = new Catalogo();
+
+    private RepositorioMusicas() {
+    }
+
+    static public void iniciar() {
+        Genero genero = new Genero(1, "Rock");
+
+        catalogo.adicionarMusica(new Musica(1, genero, "Foo Fighters",
+                "Everlong", 1997,  4.50));
+        catalogo.adicionarMusica(new Musica(1, genero, "Foo Fighters",
+            "In the lcear", 2014,  4.05));
+    }
+
+    public static Catalogo getCatalogo() {
+        return catalogo;
+    }
+}
