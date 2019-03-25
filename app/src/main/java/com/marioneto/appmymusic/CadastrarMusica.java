77fcc87b6336a89a1841827b726eef886f82e630
MarioGN <mario.geroldineto@gmail.com>
2019-03-25 15:28:12 -0300

    criando o seekbar do ano de lançamento da música

--- app/src/main/java/com/marioneto/appmymusic/CadastrarMusica.java
+++ app/src/main/java/com/marioneto/appmymusic/CadastrarMusica.java
@@ -3,13 +3,17 @@ package com.marioneto.appmymusic;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.marioneto.appmymusic.bean.Genero;
 import com.marioneto.appmymusic.util.RepositorioMusicas;
 
 public class CadastrarMusica extends AppCompatActivity {
     private Spinner spnGenero;
+    private SeekBar skAno, skDuracao;
+    private TextView tvAno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +21,33 @@ public class CadastrarMusica extends AppCompatActivity {
         setContentView(R.layout.activity_cadastrar_musica);
 
         spnGenero = findViewById(R.id.spn_genero);
+        skAno = findViewById(R.id.sk_ano);
+        tvAno = findViewById(R.id.tv_ano);
+
+
         ArrayAdapter<Genero> adapter;
         adapter = new ArrayAdapter(this,
                 android.R.layout.simple_spinner_item,
                 RepositorioMusicas.getCatalogo().getListaGeneros());
-
         spnGenero.setAdapter(adapter);
+
+
+        skAno.setMax(119);
+        skAno.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tvAno.setText(""+(1900+progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
